99c8f1189f55e1897db90714fca382de8a715e9a
Julie <agnostick165@gmail.com>
2025-04-21 19:12:35 +0500

    feat(auth): add slices, implement authentication flow and protected routes

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,30 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { feedThunk, selectLoading, selectOrders } from '../../slices/feedSlice';
 
 export const Feed: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const orders: TOrder[] = useSelector(selectOrders);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
 
-  if (!orders.length) {
-    return <Preloader />;
-  }
+  const handleGetFeeds = () => {
+    dispatch(feedThunk());
+  };
 
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  useEffect(() => {
+    dispatch(feedThunk());
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <FeedUI orders={orders} handleGetFeeds={handleGetFeeds} />
+      )}
+    </>
+  );
 };
